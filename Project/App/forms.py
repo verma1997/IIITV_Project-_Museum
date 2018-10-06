61fcf2abd2a82a4489b02056cc302c4e7eafbb5c
Chetanya <chetanyashrimalie5@gmail.com>
2018-10-07 01:28:46 +0530

    updated

---
+++ Project/App/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    """Provides form for User model"""
+
+    username = forms.CharField(label='username')
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+
+class RegisterForm(forms.ModelForm):
+    """Provides form for User model"""
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label='confirm_password',
+                                       widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirm_password']
