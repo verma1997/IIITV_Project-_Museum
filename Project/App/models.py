63e54a68d6e787bc13013275dc0ded20a4be3406
vashist_99 <201752040@iiitvadodara.ac.in>
2018-10-07 03:11:49 +0530

    database error

--- Project/App/models.py
+++ Project/App/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
+from django.utils.timezone import datetime
+
 class Project(models.Model):
     course_name=models.CharField(max_length=100)
     project_name=models.CharField(max_length=100)
     project_description=models.TextField(max_length=100)
-    member=models.OneToManyField('User')
-    date=models.DateField(default=datetime.now())
-    like=model.ManyToManyField('User')
-
-
+    member=models.ForeignKey('User',on_delete=models.CASCADE)
+    date=models.DateTimeField(auto_now_add=True)
+    like=models.IntegerField(max_length=100)
--- Project/App/urls.py
+++ Project/App/urls.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
