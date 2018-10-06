df8a2ac02dce8daab2b512237d113168b3ea43e2
Chetanya <chetanyashrimalie5@gmail.com>
2018-10-07 04:40:59 +0530

    added auth models
    Signed-off-by: Chetanya <chetanyashrimalie5@gmail.com>

--- Project/App/models.py
+++ Project/App/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import datetime
+from django.utils.translation import ugettext_lazy as _
 
 class Project(models.Model):
     semester=models.CharField(max_length=100)
@@ -11,5 +12,24 @@ class Project(models.Model):
     date=models.DateTimeField(auto_now_add=True)
     like=models.IntegerField(max_length=100)
 
+    class Meta:
+        verbose_name = _("Project")
+        verbose_name_plural = _("Projects")
+
     def __str__(self):
         return self.semester + "  " + self.course_name + " " + self.project_name
+
+class Relationship(models.Model):
+    from_user = models.ForeignKey(User, related_name='to_user', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='from_user', on_delete=models.CASCADE)
+    kind = models.CharField(max_length=100, default="Mentor")
+
+    class Meta:
+        verbose_name = _("Relationship")
+        verbose_name_plural = _("Relationships")
+
+    def __str__(self):
+        return str(
+            self.from_user.name + " - " +
+            self.to_user.name + " (" +
+            self.kind.name + ")")
