4ff39dce70ead663edfc8dbe648e46cc1c4c3bf0
vashist_99 <201752040@iiitvadodara.ac.in>
2018-10-07 19:15:47 +0530

    refined

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^login/$', views.LoginFormView.as_view(), name='login'),
     url(r'^logout/$', views.signout, name='logout'),
     url(r'^about-us/$', views.AboutUs, name='aboutus'),
-    url(r'^project-form/$', views.ProjectFormView.as_view(), name='project-form')
+    url(r'^project-form/$', views.ProjectFormView.as_view(), name='project-form'),
+    url(r'^project-detail/(?P<pk>[0-9]+)/', views.project_detail, name='project-detail')
 ]
